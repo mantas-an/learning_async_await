8ccde791eea3e497df74c2040aaf5390f02104c8
Mantas An <mantasantanavic@gmail.com>
2025-07-19 14:47:39 +0300

    Issprestos 2 uzduotis su async ir await, taip pat 1 basic su http aiohttp

--- bandymas.py
+++ bandymas.py
@@ -1,4 +1,7 @@
 import asyncio
+import  aiohttp
+from aiohttp import ClientSession
+
 
 #uzduotis
 
@@ -10,23 +13,89 @@ import asyncio
 # Use asyncio.sleep() for delays.
 
 #Kurta ziurint i uzrasus is main.py vietos kurios reikejo paziureti,pradedant - >DEF MAIN .
+# -------------------------------- 2025-07-18------------------------------
+# async def uzduotis1():
+#     print('Task 1 started')
+#     await asyncio.sleep(2)
+#     print('Task 1 finished')
+#
+# async def uzduotis2():
+#     print('Task 2 started')
+#     await asyncio.sleep(1)
+#     print('Task 2 finished')
+#
+# async def main():
+#     task1 = asyncio.create_task(uzduotis1())
+#     task2 = asyncio.create_task(uzduotis2())
+#
+#     await task1
+#     await task2
+#
+#
+# asyncio.run(main())
 
-async def uzduotis1():
-    print('Task 1 started')
-    await asyncio.sleep(2)
-    print('Task 1 finished')
+# -------------------------------- 2025-07-19------------------------------
 
-async def uzduotis2():
-    print('Task 2 started')
-    await asyncio.sleep(1)
-    print('Task 2 finished')
+
+# 1. Create an async function that prints "Tick" every second for 5 seconds
+#
+# async def spausdinti():
+#     for i in range(5):
+#         print("Tick")
+#         await asyncio.sleep(1)
+#
+# async def main():
+#     task1 = asyncio.create_task(spausdinti())
+#     await task1
+#
+# asyncio.run(main())
+
+#Nezinojau tiksliai kur for ideti, pasinaudojau Ai.
+#uZSTRIGAU TIES async def, await buvo iskart po for, todel mete 5 iskart.
+
+
+# 2. Modify your existing code to run 3 tasks simultaneously with different sleep durations
+#Make the main function gather all results.
+
+# async def spausdinti1(pavadinimas, laukimas):
+#
+#         print(pavadinimas)
+#         await asyncio.sleep(laukimas)
+#         print(f"{pavadinimas} baige")
+#
+#
+#
+# async def main():
+#     task1 = asyncio.create_task(spausdinti1("tick1", 1))
+#     task2 = asyncio.create_task(spausdinti1("tick2", 3))
+#     task3 = asyncio.create_task(spausdinti1("tick3", 2))
+#
+#     # await task1
+#     # await task2
+#     # await task3
+#     await asyncio.gather(task1, task2, task3) #gather naudojam norint surinkti visus await ir maziau rasyti reikes :)
+#
+# asyncio.run(main())
+
+
+
+
+
+
+# Simple HTTP Request:
+#
+# Fetch data from https://httpbin.org/get and print the response
+
+async def fetch_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
 
 async def main():
-    task1 = asyncio.create_task(uzduotis1())
-    task2 = asyncio.create_task(uzduotis2())
+    urls =  "https://httpbin.org/get"
 
-    await task1
-    await task2
 
+    result = await fetch_url(urls)
+    print (result)
 
 asyncio.run(main())
--- main.py
+++ main.py
@@ -18,40 +18,40 @@
 
 import asyncio
 
-# async def pasisveikinti(vardas, laukimo_laikas):
-#     print(f'Pradedu {vardas}...')
-#     await asyncio.sleep(laukimo_laikas) # simuliuojam uzduoti
-#     print(f'{vardas} baige darbus!')
-#
-#
-# async def main():
-#     # 3 funkciju paleidimas
-#     task1 = asyncio.create_task(pasisveikinti('Funkcija a', 3))
-#     task2 = asyncio.create_task(pasisveikinti('Funkcija b', 2))
-#     task3 = asyncio.create_task(pasisveikinti('Funkcija c', 1))
-#
-#     await task1
-#     await task2
-#     await task3
-#
-# #programos paleidimas
-# asyncio.run(main())
+async def pasisveikinti(vardas, laukimo_laikas):
+    print(f'Pradedu {vardas}...')
+    await asyncio.sleep(laukimo_laikas) # simuliuojam uzduoti
+    print(f'{vardas} baige darbus!')
+
+
+async def main():
+    # 3 funkciju paleidimas
+    task1 = asyncio.create_task(pasisveikinti('Funkcija a', 3))
+    task2 = asyncio.create_task(pasisveikinti('Funkcija b', 2))
+    task3 = asyncio.create_task(pasisveikinti('Funkcija c', 1))
+
+    await task1
+    await task2
+    await task3
+
+#programos paleidimas
+asyncio.run(main())
 
 
 
 
 #            COROUTINE
 
-async def main():
-    #async itraukia funkcija i deze, kuria pakvietus grazins COROUTINE objekta. kad execute Coroutine mum reikes await.
-    #await turi buti viduje Asinchronineje funkcijoje
-    print('Mantas')
-    await dede("tekstas") #await sioje vietoje neleidzia pereiti prie kito print.
-    print("Pabaigta") #nustacius aysncio.sleep gauname sia zinute.
-
-async def dede(text):
-    print(text)
-    await asyncio.sleep(5)
-
-#main() # jeigu be await gausime warning coroutine 'main' was never awaited main()
-asyncio.run(main()) #kad paleistume asinchronine coroutine. P.S Tai sukuria event loop.
+# async def main():
+#     #async itraukia funkcija i deze, kuria pakvietus grazins COROUTINE objekta. kad execute Coroutine mum reikes await.
+#     #await turi buti viduje Asinchronineje funkcijoje
+#     print('Mantas')
+#     await dede("tekstas") #await sioje vietoje neleidzia pereiti prie kito print.
+#     print("Pabaigta") #nustacius aysncio.sleep gauname sia zinute.
+#
+# async def dede(text):
+#     print(text)
+#     await asyncio.sleep(5)
+#
+# #main() # jeigu be await gausime warning coroutine 'main' was never awaited main()
+# asyncio.run(main()) #kad paleistume asinchronine coroutine. P.S Tai sukuria event loop.
